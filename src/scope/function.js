c734d41f5c863a2175dc24f98d778f43855e59d8
Nestor Gonzalez <nestor.ge222@gmail.com>
2021-11-16 03:51:32 +0000

    page scope

---
+++ src/scope/function.js
@@ -0,0 +1,19 @@
+const fruits = ()=>{
+    var fluit = 'apple';
+    console.log(fluit);
+
+}
+
+fruits();
+
+const anotherFunction = ()=>{
+    var x =1;
+    var x =2;
+    let y =1;
+    //let y =2; -> no se puede redeclara un y
+
+    console.log(x);
+    console.log(y);
+}
+
+anotherFunction();
