0a1f7038d9cac0ff57eceef28b74264b1ce6268b
Nestor Gonzalez <nestor.ge222@gmail.com>
2021-11-16 03:37:20 +0000

    clase numero 2 del curso Globales

---
+++ src/scope/global.js
@@ -0,0 +1,31 @@
+var hello = 'hello';
+
+let world = 'hello world';
+
+
+const helloWorld = 'Hello World!';
+
+
+const anotherFunction = ()=>{
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+
+}
+
+anotherFunction();
+
+//esta es una mala practica 
+const helloWorld = ()=>{
+    globalVar = 'im global'
+}
+helloWorld();
+console.log(globalVar);
+
+
+const anotherFunction2=()=>{
+    var localVar = globalVar = 'Im a global'
+
+}
+
+
