ed83aa35b7cdabe658fad01e5a1197a993d7aaa8
Nestor Gonzalez <nestor.ge222@gmail.com>
2021-11-17 03:26:55 +0000

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,18 @@
+a=2;
+var a;
+console.log(a)
+//el codigo de var a se pasa a primera linea por la elevacion en jsj
+
+
+    console.log(a)
+    var a =2;
+//pero cuando declaro a despues de console entonces ahi si da error
+
+
+//en la variable pasa lo mismo que la primera opcion ya que el motor complila el codigo antes de ser interpretado por lo que pasa todas las declaraciones al inicio
+nameOfDog('boby');
+function nameOfDog(name){
+    console.log(name)
+}
+
+
